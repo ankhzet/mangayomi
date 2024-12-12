3ddfde5bcaf0eb42597bbe9ad0778e344d323a67
Yegor Shovkun <ankhzet@gmail.com>
2024-12-13 00:02:40 +0200

    fix: patch Comick parse results

--- lib/services/get_chapter_pages.dart
+++ lib/services/get_chapter_pages.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mangayomi/eval/dart/service.dart';
 import 'package:mangayomi/eval/javascript/http.dart';
 import 'package:mangayomi/eval/javascript/service.dart';
@@ -11,11 +13,10 @@ import 'package:mangayomi/models/settings.dart';
 import 'package:mangayomi/models/source.dart';
 import 'package:mangayomi/modules/manga/archive_reader/providers/archive_reader_providers.dart';
 import 'package:mangayomi/modules/manga/reader/reader_view.dart';
+import 'package:mangayomi/modules/more/providers/incognito_mode_state_provider.dart';
 import 'package:mangayomi/providers/storage_provider.dart';
 import 'package:mangayomi/utils/utils.dart';
-import 'package:mangayomi/modules/more/providers/incognito_mode_state_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'get_chapter_pages.g.dart';
 
@@ -31,32 +32,56 @@ class GetChapterPagesModel {
   });
 }
 
+// fixme: remove this when extension is patched https://github.com/kodjodevf/mangayomi/issues/228
+List<PageUrl> patchPages(Source source, List<PageUrl> pages) {
+  if (source.typeSource != 'comick') {
+    return pages;
+  }
+
+  return pages.fold([], (accumulator, item) {
+    if (item.url.length <= 4) {
+      accumulator[accumulator.length - 1].url += '_.${item.url}';
+    } else {
+      accumulator.add(item);
+    }
+
+    return accumulator;
+  });
+}
+
 @riverpod
 Future<GetChapterPagesModel> getChapterPages(
   Ref ref, {
   required Chapter chapter,
 }) async {
-  List<UChapDataPreload> uChapDataPreload = [];
-  List<PageUrl> pageUrls = [];
-  List<bool> isLocalList = [];
+  final manga = chapter.manga.value!;
   final settings = isar.settings.getSync(227);
   List<ChapterPageurls>? chapterPageUrlsList = settings!.chapterPageUrlsList ?? [];
   final isarPageUrls = chapterPageUrlsList.where((element) => element.chapterId == chapter.id);
   final incognitoMode = ref.watch(incognitoModeStateProvider);
   final chapterDirectory = await StorageProvider.getMangaChapterDirectory(chapter);
-  final mangaDirectory = await StorageProvider.getMangaMainDirectory(chapter.manga.value!);
-
+  final mangaDirectory = await StorageProvider.getMangaMainDirectory(manga);
+  List<UChapDataPreload> uChapDataPreload = [];
+  List<bool> isLocalList = [];
+  List<PageUrl> pageUrls = [];
   List<Uint8List?> archiveImages = [];
   final isLocalArchive = (chapter.archivePath ?? '').isNotEmpty;
-  if (!chapter.manga.value!.isLocalArchive!) {
-    final source = getSource(chapter.manga.value!.lang!, chapter.manga.value!.source!)!;
-    if (isarPageUrls.isNotEmpty && isarPageUrls.first.urls != null && isarPageUrls.first.urls!.isNotEmpty) {
-      for (var i = 0; i < isarPageUrls.first.urls!.length; i++) {
+
+
+  if (!manga.isLocalArchive!) {
+    final source = getSource(manga.lang!, manga.source!)!;
+    final data = isarPageUrls.isNotEmpty ? isarPageUrls.first : null;
+    final urls = data?.urls;
+
+    if (urls?.isNotEmpty ?? false) {
+      for (var i = 0; i < urls!.length; i++) {
         Map<String, String>? headers;
-        if (isarPageUrls.first.headers?.isNotEmpty ?? false) {
-          headers = (jsonDecode(isarPageUrls.first.headers![i]) as Map?)?.toMapStringString;
+
+        if (data!.headers?.isNotEmpty ?? false) {
+          headers = (jsonDecode(data.headers![i]) as Map?)?.toMapStringString;
         }
-        pageUrls.add(PageUrl(isarPageUrls.first.urls![i], headers: headers));
+
+        pageUrls.add(PageUrl(urls[i], headers: headers));
       }
     } else {
       if (source.sourceCodeLanguage == SourceCodeLanguage.dart) {
@@ -65,6 +90,8 @@ Future<GetChapterPagesModel> getChapterPages(
         pageUrls = await JsExtensionService(source).getPageList(chapter.url!);
       }
     }
+
+    pageUrls = patchPages(source, pageUrls);
   }
 
   if (pageUrls.isNotEmpty || isLocalArchive) {

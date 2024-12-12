571150fb02f81aa499544cb24052217ef83828c0
Yegor Shovkun <ankhzet@gmail.com>
2024-12-12 21:05:26 +0200

    refactor: extract Navbar component

--- lib/modules/main_view/main_screen.dart
+++ lib/modules/main_view/main_screen.dart
@@ -7,6 +7,7 @@ import 'package:isar/isar.dart';
 import 'package:mangayomi/main.dart';
 import 'package:mangayomi/models/update.dart';
 import 'package:mangayomi/models/source.dart';
+import 'package:mangayomi/modules/main_view/widgets/navbar.dart';
 import 'package:mangayomi/modules/widgets/loading_icon.dart';
 import 'package:mangayomi/services/fetch_anime_sources.dart';
 import 'package:mangayomi/services/fetch_manga_sources.dart';
@@ -25,17 +26,6 @@ class MainScreen extends ConsumerWidget {
 
   final Widget content;
 
-  String getHyphenatedUpdatesLabel(String languageCode, String defaultLabel) {
-    switch (languageCode) {
-      case 'de': return "Aktuali-\nsierungen";
-      case 'es':
-      case 'es_419': return "Actuali-\nzaciones";
-      case 'it': return "Aggiorna-\nmenti";
-      case 'tr': return "Güncel-\nlemeler";
-      default: return defaultLabel;
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = l10nLocalizations(context)!;
@@ -51,17 +41,9 @@ class MainScreen extends ConsumerWidget {
         );
         bool isReadingScreen =
             location == '/mangareaderview' || location == '/animePlayerView';
-        int currentIndex = switch (location) {
-          null || '/MangaLibrary' => 0,
-          '/AnimeLibrary' => 1,
-          '/updates' => 2,
-          '/history' => 3,
-          '/browse' => 4,
-          _ => 5,
-        };
 
         final incognitoMode = ref.watch(incognitoModeStateProvider);
-        final isLongPressed = ref.watch(isLongPressedMangaStateProvider);
+
         return Column(
           children: [
             if (!isReadingScreen)
@@ -98,220 +80,14 @@ class MainScreen extends ConsumerWidget {
                 body: context.isTablet
                     ? Row(
                         children: [
-                          AnimatedContainer(
-                            duration: const Duration(milliseconds: 0),
-                            width: switch (isLongPressed) {
-                              true => 0,
-                              _ => switch (location) {
-                                  null => 100,
-                                  != '/MangaLibrary' &&
-                                        != '/AnimeLibrary' &&
-                                        != '/history' &&
-                                        != '/updates' &&
-                                        != '/browse' &&
-                                        != '/more' =>
-                                    0,
-                                  _ => 100,
-                                },
-                            },
-                            child: Stack(
-                              children: [
-                                NavigationRailTheme(
-                                  data: NavigationRailThemeData(
-                                    indicatorShape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(30)),
-                                  ),
-                                  child: Builder(builder: (context) {
-                                    return NavigationRail(
-                                      labelType: NavigationRailLabelType.all,
-                                      useIndicator: true,
-                                      destinations: [
-                                        NavigationRailDestination(
-                                            selectedIcon: const Icon(
-                                                Icons.collections_bookmark),
-                                            icon: const Icon(Icons
-                                                .collections_bookmark_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Text(l10n.manga))),
-                                        NavigationRailDestination(
-                                            selectedIcon: const Icon(
-                                                Icons.video_collection),
-                                            icon: const Icon(Icons
-                                                .video_collection_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Text(l10n.anime))),
-                                        NavigationRailDestination(
-                                            selectedIcon:
-                                                const Icon(Icons.new_releases),
-                                            icon: const Icon(
-                                                Icons.new_releases_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Stack(
-                                                  children: [
-                                                    Text(
-                                                      getHyphenatedUpdatesLabel(
-                                                        ref.watch(l10nLocaleStateProvider).languageCode,
-                                                        l10n.updates,
-                                                      ),
-                                                      textAlign: TextAlign.center,
-                                                    ),
-                                                  ],
-                                                ))),
-                                        NavigationRailDestination(
-                                            selectedIcon:
-                                                const Icon(Icons.history),
-                                            icon: const Icon(
-                                                Icons.history_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Text(l10n.history))),
-                                        NavigationRailDestination(
-                                            selectedIcon:
-                                                const Icon(Icons.explore),
-                                            icon: const Icon(
-                                                Icons.explore_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Text(l10n.browse))),
-                                        NavigationRailDestination(
-                                            selectedIcon:
-                                                const Icon(Icons.more_horiz),
-                                            icon: const Icon(
-                                                Icons.more_horiz_outlined),
-                                            label: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 5),
-                                                child: Text(l10n.more))),
-                                      ],
-                                      selectedIndex: currentIndex,
-                                      onDestinationSelected: (newIndex) {
-                                        final fn = switch (newIndex) {
-                                          0 => route.go('/MangaLibrary'),
-                                          1 => route.go('/AnimeLibrary'),
-                                          2 => route.go('/updates'),
-                                          3 => route.go('/history'),
-                                          4 => route.go('/browse'),
-                                          _ => route.go('/more'),
-                                        };
-                                        fn;
-                                      },
-                                    );
-                                  }),
-                                ),
-                                Positioned(
-                                    right: 18,
-                                    top: 140,
-                                    child: _updatesTotalNumbers(ref)),
-                                Positioned(
-                                    right: 18,
-                                    top: 275,
-                                    child: _extensionUpdateTotalNumbers(ref)),
-                              ],
-                            ),
-                          ),
+                          const Navbar(horizontal: false),
                           Expanded(child: content)
                         ],
                       )
                     : content,
                 bottomNavigationBar: context.isTablet
                     ? null
-                    : AnimatedContainer(
-                        duration: const Duration(milliseconds: 0),
-                        width: context.width(1),
-                        height: switch (isLongPressed) {
-                          true => 0,
-                          _ => switch (location) {
-                              null => null,
-                              != '/MangaLibrary' &&
-                                    != '/AnimeLibrary' &&
-                                    != '/history' &&
-                                    != '/updates' &&
-                                    != '/browse' &&
-                                    != '/more' =>
-                                0,
-                              _ => null,
-                            },
-                        },
-                        child: NavigationBarTheme(
-                          data: NavigationBarThemeData(
-                            indicatorShape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                          ),
-                          child: NavigationBar(
-                            animationDuration:
-                                const Duration(milliseconds: 500),
-                            selectedIndex: currentIndex,
-                            destinations: [
-                              NavigationDestination(
-                                  selectedIcon:
-                                      const Icon(Icons.collections_bookmark),
-                                  icon: const Icon(
-                                      Icons.collections_bookmark_outlined),
-                                  label: l10n.manga),
-                              NavigationDestination(
-                                  selectedIcon:
-                                      const Icon(Icons.video_collection),
-                                  icon: const Icon(
-                                      Icons.video_collection_outlined),
-                                  label: l10n.anime),
-                              Stack(
-                                children: [
-                                  NavigationDestination(
-                                      selectedIcon:
-                                          const Icon(Icons.new_releases),
-                                      icon: const Icon(
-                                          Icons.new_releases_outlined),
-                                      label: l10n.updates),
-                                  Positioned(
-                                      right: 14,
-                                      top: 3,
-                                      child: _updatesTotalNumbers(ref)),
-                                ],
-                              ),
-                              NavigationDestination(
-                                  selectedIcon: const Icon(Icons.history),
-                                  icon: const Icon(Icons.history_outlined),
-                                  label: l10n.history),
-                              Stack(
-                                children: [
-                                  NavigationDestination(
-                                      selectedIcon: const Icon(Icons.explore),
-                                      icon: const Icon(Icons.explore_outlined),
-                                      label: l10n.browse),
-                                  Positioned(
-                                      right: 14,
-                                      top: 3,
-                                      child: _extensionUpdateTotalNumbers(ref)),
-                                ],
-                              ),
-                              NavigationDestination(
-                                  selectedIcon: const Icon(Icons.more_horiz),
-                                  icon: const Icon(Icons.more_horiz_outlined),
-                                  label: l10n.more),
-                            ],
-                            onDestinationSelected: (newIndex) {
-                              final fn = switch (newIndex) {
-                                0 => route.go('/MangaLibrary'),
-                                1 => route.go('/AnimeLibrary'),
-                                2 => route.go('/updates'),
-                                3 => route.go('/history'),
-                                4 => route.go('/browse'),
-                                _ => route.go('/more'),
-                              };
-                              fn;
-                            },
-                          ),
-                        ),
-                      ),
+                    : const Navbar(horizontal: true),
               ),
             ),
           ],
@@ -323,73 +99,4 @@ class MainScreen extends ConsumerWidget {
       return const LoadingIcon();
     });
   }
-}
-
-Widget _extensionUpdateTotalNumbers(WidgetRef ref) {
-  return StreamBuilder(
-      stream: isar.sources
-          .filter()
-          .idIsNotNull()
-          .and()
-          .isActiveEqualTo(true)
-          .watch(fireImmediately: true),
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          final entries = snapshot.data!
-              .where((element) =>
-                  compareVersions(element.version!, element.versionLast!) < 0)
-              .toList();
-          return entries.isEmpty
-              ? Container()
-              : Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color.fromARGB(255, 176, 46, 37)),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-                    child: Text(
-                      entries.length.toString(),
-                      style: TextStyle(
-                          fontSize: 10,
-                          color: Theme.of(context).textTheme.bodySmall!.color),
-                    ),
-                  ),
-                );
-        }
-        return Container();
-      });
-}
-
-Widget _updatesTotalNumbers(WidgetRef ref) {
-  return StreamBuilder(
-      stream: isar.updates.filter().idIsNotNull().watch(fireImmediately: true),
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          final entries = snapshot.data!.where((element) {
-            if (!element.chapter.isLoaded) {
-              element.chapter.loadSync();
-            }
-            return !(element.chapter.value?.isRead ?? false);
-          }).toList();
-          return entries.isEmpty
-              ? Container()
-              : Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color.fromARGB(255, 176, 46, 37)),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-                    child: Text(
-                      entries.length.toString(),
-                      style: TextStyle(
-                          fontSize: 10,
-                          color: Theme.of(context).textTheme.bodySmall!.color),
-                    ),
-                  ),
-                );
-        }
-        return Container();
-      });
 }
---
+++ lib/modules/main_view/widgets/navbar.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:isar/isar.dart';
+import 'package:mangayomi/main.dart';
+import 'package:mangayomi/models/chapter.dart';
+import 'package:mangayomi/models/source.dart';
+import 'package:mangayomi/models/update.dart';
+import 'package:mangayomi/models/update_group.dart';
+import 'package:mangayomi/modules/library/providers/library_state_provider.dart';
+import 'package:mangayomi/modules/main_view/widgets/update_badge.dart';
+import 'package:mangayomi/providers/l10n_providers.dart';
+import 'package:mangayomi/router/router.dart';
+import 'package:mangayomi/services/fetch_sources_list.dart';
+import 'package:mangayomi/utils/extensions/build_context_extensions.dart';
+
+const double minVerticalWidth = 100;
+
+class Navbar extends StatelessWidget {
+  final bool horizontal;
+
+  const Navbar({
+    super.key,
+    required this.horizontal,
+  });
+
+  static String getHyphenatedUpdatesLabel(String languageCode, String defaultLabel) {
+    switch (languageCode) {
+      case 'de':
+        return "Aktuali-\nsierungen";
+      case 'es':
+      case 'es_419':
+        return "Actuali-\nzaciones";
+      case 'it':
+        return "Aggiorna-\nmenti";
+      case 'tr':
+        return "Güncel-\nlemeler";
+      default:
+        return defaultLabel;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final isLongPressed = ref.watch(isLongPressedMangaStateProvider);
+
+      if (isLongPressed || (context.isTablet && horizontal)) {
+        return Container();
+      }
+
+      final items = _buildItems(context, ref);
+
+      int? locationIndex = _getLocation(context, ref, items);
+      int currentIndex = locationIndex ?? 0;
+
+      double? sized(bool horizontal, double value) {
+        if (locationIndex == null) {
+          return 0;
+        }
+
+        if (context.isTablet) {
+          return horizontal ? value : null;
+        }
+
+        return horizontal ? null : value;
+      }
+
+      double? width = sized(true, minVerticalWidth);
+      double? height = sized(false, 64);
+
+      return AnimatedContainer(
+        duration: const Duration(milliseconds: 0),
+        width: width,
+        height: height,
+        child: horizontal
+            ? _buildHorizontal(ref: ref, context: context, items: items, index: currentIndex)
+            : _buildVertical(ref: ref, context: context, items: items, index: currentIndex),
+      );
+    });
+  }
+
+  Widget _buildHorizontal({
+    required WidgetRef ref,
+    required BuildContext context,
+    required List<NavItem> items,
+    required int index,
+  }) {
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(
+        indicatorShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+      child: NavigationBar(
+        animationDuration: const Duration(milliseconds: 500),
+        selectedIndex: index,
+        onDestinationSelected: _navigate(context, items),
+        destinations: items.map((NavItem item) {
+          final widget = NavigationDestination(
+            icon: item.icon,
+            selectedIcon: item.selectedIcon,
+            label: item.label,
+          );
+
+          if (item.badge != null) {
+            return Stack(
+              children: [
+                widget,
+                Positioned(right: 14, top: 3, child: item.badge!),
+              ],
+            );
+          }
+
+          return widget;
+        }).toList(growable: false),
+      ),
+    );
+  }
+
+  Widget _buildVertical({
+    required WidgetRef ref,
+    required BuildContext context,
+    required List<NavItem> items,
+    required int index,
+  }) {
+    final badges = items.indexed
+        .map((entry) => entry.$2.badge != null
+            ? Positioned(left: minVerticalWidth / 2, top: 4 + entry.$1 * 70, child: entry.$2.badge!)
+            : null)
+        .whereType<Widget>();
+
+    return Stack(children: [
+      NavigationRailTheme(
+        data: NavigationRailThemeData(
+          indicatorShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        ),
+        child: NavigationRail(
+          labelType: NavigationRailLabelType.all,
+          useIndicator: true,
+          selectedIndex: index,
+          onDestinationSelected: _navigate(context, items),
+          destinations: items
+              .map((NavItem item) => NavigationRailDestination(
+                    icon: item.icon,
+                    selectedIcon: item.selectedIcon,
+                    label: Padding(padding: const EdgeInsets.only(top: 5), child: Text(item.label)),
+                  ))
+              .toList(growable: false),
+        ),
+      ),
+      ...badges,
+    ]);
+  }
+
+  int? _getLocation(BuildContext context, WidgetRef ref, List<NavItem> items) {
+    final location = ref.watch(routerCurrentLocationStateProvider(context));
+
+    if (location == null) {
+      return 0;
+    }
+
+    final index = items.indexWhere((item) => item.route == location);
+
+    return index < 0 ? null : index;
+  }
+
+  void Function(int index) _navigate(BuildContext context, List<NavItem> items) {
+    return (int index) => GoRouter.of(context).go(items[index].route);
+  }
+
+  List<NavItem> _buildItems<T>(BuildContext context, WidgetRef ref) {
+    final l10n = l10nLocalizations(context)!;
+    final lang = ref.watch(l10nLocaleStateProvider).languageCode;
+
+    return [
+      NavItem(
+        selectedIcon: const Icon(Icons.collections_bookmark),
+        icon: const Icon(Icons.collections_bookmark_outlined),
+        label: l10n.manga,
+        route: '/MangaLibrary',
+      ),
+      NavItem(
+        selectedIcon: const Icon(Icons.video_collection),
+        icon: const Icon(Icons.video_collection_outlined),
+        label: l10n.anime,
+        route: '/AnimeLibrary',
+      ),
+      NavItem(
+        selectedIcon: const Icon(Icons.new_releases),
+        icon: const Icon(Icons.new_releases_outlined),
+        label: context.isTablet ? getHyphenatedUpdatesLabel(lang, l10n.updates) : l10n.updates,
+        route: '/updates',
+        badge: _updatesTotalNumbers(ref),
+      ),
+      NavItem(
+        selectedIcon: const Icon(Icons.history),
+        icon: const Icon(Icons.history_outlined),
+        label: l10n.history,
+        route: '/history',
+      ),
+      NavItem(
+        selectedIcon: const Icon(Icons.explore),
+        icon: const Icon(Icons.explore_outlined),
+        label: l10n.browse,
+        route: '/browse',
+        badge: _extensionUpdateTotalNumbers(ref),
+      ),
+      NavItem(
+        selectedIcon: const Icon(Icons.more_horiz),
+        icon: const Icon(Icons.more_horiz_outlined),
+        label: l10n.more,
+        route: '/more',
+      ),
+    ];
+  }
+}
+
+Widget _extensionUpdateTotalNumbers(WidgetRef ref) {
+  return StreamBuilder(
+    stream: isar.sources.filter().idIsNotNull().and().isActiveEqualTo(true).watch(fireImmediately: true),
+    builder: (context, snapshot) => UpdateBadge(
+      count: ((snapshot.hasData && snapshot.data!.isNotEmpty)
+          ? snapshot.data!.where((element) => compareVersions(element.version!, element.versionLast!) < 0).length
+          : 0),
+    ),
+  );
+}
+
+Widget _updatesTotalNumbers(WidgetRef ref) {
+  return StreamBuilder(
+    stream:
+        isar.updates.filter().idIsNotNull().chapter((q) => q.not().isReadEqualTo(true)).watch(fireImmediately: true),
+    builder: (context, snapshot) {
+      int count = 0;
+
+      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+        final groups = UpdateGroup.groupUpdates(
+          snapshot.data!,
+              (Update update) => '${update.mangaId!}-${update.chapter.value!.order}',
+        );
+
+        count = groups.length;
+      }
+
+      return UpdateBadge(
+        count: count,
+      );
+    },
+
+    // builder: (context, snapshot) => UpdateBadge(
+    //   count: snapshot.hasData && snapshot.data!.isNotEmpty
+    //       ? UpdateGroup.groupUpdates(
+    //           snapshot.data!,
+    //           (Update update) => '${update.mangaId!}-${update.chapter.value!.order}',
+    //         ).length
+    //       : 0,
+    // ),
+  );
+}
+
+class NavItem {
+  final String route;
+  final String label;
+  final Widget icon;
+  final Widget selectedIcon;
+  final Widget? badge;
+
+  const NavItem({
+    required this.route,
+    required this.label,
+    required this.icon,
+    required this.selectedIcon,
+    this.badge,
+  });
+}

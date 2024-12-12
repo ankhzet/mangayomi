adbba5c10a95efdf06132c0eb651743f523d7f97
Yegor Shovkun <ankhzet@gmail.com>
2024-12-12 15:19:51 +0200

    refactor: extract genres badges widget

---
+++ lib/modules/manga/detail/widgets/genre_badge_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:mangayomi/utils/extensions/build_context_extensions.dart';
+
+class GenreBadgeWidget extends StatelessWidget {
+  final String genre;
+  final double height;
+  final void Function()? onPressed;
+
+  const GenreBadgeWidget({
+    super.key,
+    required this.genre,
+    this.height = 30,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: Colors.grey.withOpacity(0.2),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          genre,
+          style: TextStyle(fontSize: 11.5, color: context.isLight ? Colors.black : Colors.white),
+        ),
+      ),
+    );
+  }
+}

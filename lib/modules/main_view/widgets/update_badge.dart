40d1a2ebeb06b2f08553daf15bc45dbdf66878b4
Yegor Shovkun <ankhzet@gmail.com>
2024-12-12 21:05:26 +0200

    refactor: extract Navbar component

---
+++ lib/modules/main_view/widgets/update_badge.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:mangayomi/utils/extensions/build_context_extensions.dart';
+
+class UpdateBadge extends StatelessWidget {
+  final int count;
+  final Color color;
+
+  const UpdateBadge({
+    super.key,
+    required this.count,
+    this.color = const Color.fromARGB(255, 176, 46, 37),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (count <= 0) {
+      return Container();
+    }
+
+    return IgnorePointer(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color.withOpacity(0.6),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+          child: Text(
+            count.toString(),
+            style: TextStyle(
+                fontSize: Theme.of(context).textTheme.labelSmall?.fontSize ?? 10, color: context.dynamicBlackWhiteColor),
+          ),
+        ),
+      ),
+    );
+  }
+}

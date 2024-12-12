fa06362531ab4b7067b60ecfd81c1f525ff0d89a
Yegor Shovkun <ankhzet@gmail.com>
2024-12-12 20:59:57 +0200

    fix: fix manga selectors

--- lib/modules/library/providers/isar_providers.dart
+++ lib/modules/library/providers/isar_providers.dart
@@ -4,48 +4,36 @@ import 'package:mangayomi/models/manga.dart';
 import 'package:mangayomi/models/settings.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 part 'isar_providers.g.dart';
 
 @riverpod
-Stream<List<Manga>> getAllMangaStream(Ref ref,
-    {required int? categoryId, required bool? isManga}) async* {
+Stream<List<Manga>> getAllMangaStream(Ref ref, {required int? categoryId, required bool? isManga}) async* {
   yield* categoryId == null
-      ? isar.mangas
-          .filter()
-          .favoriteEqualTo(true)
-          .and()
-          .isMangaEqualTo(isManga)
-          .watch(fireImmediately: true)
+      ? isar.mangas.filter().favoriteEqualTo(true).and().isMangaEqualTo(isManga).watch(fireImmediately: true)
       : isar.mangas
           .filter()
           .favoriteEqualTo(true)
-          .categoriesIsNotEmpty()
-          .categoriesElementEqualTo(categoryId)
           .and()
           .isMangaEqualTo(isManga)
+          .and()
+          .categoriesElementEqualTo(categoryId)
           .watch(fireImmediately: true);
 }
 
 @riverpod
-Stream<List<Manga>> getAllMangaWithoutCategoriesStream(Ref ref,
-    {required bool? isManga}) async* {
+Stream<List<Manga>> getAllMangaWithoutCategoriesStream(Ref ref, {required bool? isManga}) async* {
   yield* isar.mangas
       .filter()
       .favoriteEqualTo(true)
+      .and()
       .isMangaEqualTo(isManga)
       .and()
-      .categoriesIsEmpty()
-      .or()
-      .categoriesIsNull()
+      .group((q) => q.categoriesIsEmpty().or().categoriesIsNull())
       .watch(fireImmediately: true);
 }
 
 @riverpod
 Stream<List<Settings>> getSettingsStream(Ref ref) async* {
-  yield* isar.settings
-      .filter()
-      .idIsNotNull()
-      .and()
-      .idEqualTo(227)
-      .watch(fireImmediately: true);
+  yield* isar.settings.filter().idEqualTo(227).watch(fireImmediately: true);
 }
